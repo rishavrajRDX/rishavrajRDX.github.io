db3fe2d18a7068f7d6e7ce1af599662783000523
Rishav Raj <raj.rishav12148@gmail.com>
2020-08-01 19:54:03 +0530

    tenth commit

--- js/neww.js
+++ js/neww.js
@@ -1,33 +1,3 @@
-// loader
-
-
-  $(document).ready(function(){
-  var docEl = $(document),
-      headerEl = $('header'),
-      headerWrapEl = $('.wrapHead'),
-      navEl = $('nav'),
-      linkScroll = $('.scroll');
-  
-  docEl.on('scroll', function(){
-    if ( docEl.scrollTop() > 60 ){
-      headerEl.addClass('fixed-to-top');
-      headerWrapEl.addClass('fixed-to-top');
-      navEl.addClass('fixed-to-top');
-    }
-    else {
-      headerEl.removeClass('fixed-to-top');
-      headerWrapEl.removeClass('fixed-to-top');
-      navEl.removeClass('fixed-to-top');
-    }
-  });
-  
-  linkScroll.click(function(e){
-      e.preventDefault();
-      $('body, html').animate({
-         scrollTop: $(this.hash).offset().top
-      }, 500);
-   });
-});
 
 
 // navbar
@@ -42,10 +12,6 @@
 
 
 
-// window.addEventListener("load",function(){
-//   var load_screen=document.querySelector(".load-screen");
-//   document.body.removChild(load_screen);
-// })
 
 window.addEventListener("load",function(){
   console.log("loaded");
